ff76b2d3fb0459f251710e3ab44e2a40dcd8ae58
JamieAvery <123706227+JamieAvery@users.noreply.github.com>
2024-02-07 10:53:46 -0500

    python file

---
+++ hello.py
@@ -0,0 +1,14 @@
+#for num in range(1,20):
+#    print(num)
+
+#def addNumbers(num1, num2):
+#    sum = num1 + num2
+#    print(sum)
+#   return
+#addNumbers(2,3)
+#addNumbers(4,5)
+
+str = " "
+iter = ("I", "am", "awesome.")
+a = str.join(iter)
+print(a)
